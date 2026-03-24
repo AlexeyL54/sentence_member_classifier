1d854a886da4e2f6a9143616a5391a19ffcdaafd
Alexey <alexpushkins@yandex.ru>
2026-03-24 08:18:35 +0300

    add tests, start inference refactoring

---
+++ tests/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/link.d"
+  "CMakeFiles/test.dir/back/test_tokenizer.cpp.o"
+  "CMakeFiles/test.dir/back/test_tokenizer.cpp.o.d"
+  "CMakeFiles/test.dir/home/alexey/programming/ML/sentence_member_classifier/src/back/simple_tokenizer.cpp.o"
+  "CMakeFiles/test.dir/home/alexey/programming/ML/sentence_member_classifier/src/back/simple_tokenizer.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
